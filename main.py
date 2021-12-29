22ea30681de9511f36976386815ffaa286caf8a3
arbel <arbelvsr@gmail.com>
2021-12-29 17:57:35 +0200

    commit

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-import pillow
+from PIL import Image
+import numpy as np
+image = Image.open("bffs.jpeg")
+pix=np.array(image)
+print(pix.shape)
+print(pix)
+
+for()
+
+A = np.array([[3, 3, 2], [2,3,-2]])
+# perform SVD
+U, singular, V_transpose = np.linalg.svd(A, full_matrices=True, compute_uv=True)
